495d4e946df47f70c92d6bbf1201acd40c97cd52
croesusking <cha19018@byui.edu>
2021-03-10 05:58:21 +0800

    commiting claras comment

--- app/src/main/java/team4/cs246/LoginActivity.java
+++ app/src/main/java/team4/cs246/LoginActivity.java
@@ -16,7 +16,7 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-
+//Clara's comment
 public class LoginActivity extends AppCompatActivity {
     private EditText mEmail;
     private EditText mPassword;
