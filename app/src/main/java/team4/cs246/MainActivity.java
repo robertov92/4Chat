8c03eefa594f708e382464f525488e134b7966e9
robertov92 <vil13004@byui.edu>
2021-03-18 16:35:16 -0600

    Main problem, trying to fix it

--- app/src/main/java/team4/cs246/MainActivity.java
+++ app/src/main/java/team4/cs246/MainActivity.java
@@ -36,9 +36,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+
         // firebase authentication instance
         mAuth = FirebaseAuth.getInstance();
         mCurrentUserId = mAuth.getCurrentUser().getUid(); // current user id as a string
+
+        if (mCurrentUserId == null){
+            Intent startIntent = new Intent(MainActivity.this, StartActivity.class);
+            startActivity(startIntent);
+            finish();
+        }
 
         // Toolbar
         mDatabaseRef = FirebaseDatabase.getInstance().getReference();
