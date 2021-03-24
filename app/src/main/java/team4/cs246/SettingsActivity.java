386bd53e954f37ad03fc61fd12ab6ccbd0dd38bb
croesusking <cha19018@byui.edu>
2021-03-25 00:22:25 +0800

    DO NOT MERGE - end of vid 11 and beginning of 12.

--- app/src/main/java/team4/cs246/SettingsActivity.java
+++ app/src/main/java/team4/cs246/SettingsActivity.java
@@ -4,7 +4,11 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
@@ -16,6 +20,8 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public class SettingsActivity extends AppCompatActivity {
 
 
@@ -25,10 +31,30 @@ public class SettingsActivity extends AppCompatActivity {
     private Toolbar mToolbar;
 
 
+    //android layout
+
+    private CircleImageView mDisplayImage;
+    private TextView mName;
+    private TextView mStatus;
+
+    private Button mStatusBtn;
+    private Button mImageBtn;
+
+    private static final int GALLERY_PICK =1;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+
+        mDisplayImage = (CircleImageView)findViewById(R.id.settings_image);
+        mName = (TextView) findViewById(R.id.settings_display_name);
+        mStatus = (TextView) findViewById(R.id.settings_status);
+
+        mStatusBtn = (Button)findViewById(R.id.setting_status_btn);
 
         mToolbar = findViewById(R.id.settings_toolbar);
         setSupportActionBar(mToolbar);
@@ -43,7 +69,15 @@ public class SettingsActivity extends AppCompatActivity {
         mUserDatabase.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                Toast.makeText(SettingsActivity.this,snapshot.toString(),Toast.LENGTH_LONG).show();
+                String name = snapshot.child("name").getValue().toString();
+                String image = snapshot.child("image").getValue().toString();
+                String status = snapshot.child("status").getValue().toString();
+                String thumb_image = snapshot.child("thumb_image").getValue().toString();
+
+                mName.setText(name);
+                mStatus.setText(status);
+
+
 
             }
 
@@ -52,8 +86,37 @@ public class SettingsActivity extends AppCompatActivity {
 
             }
         });
+        mStatusBtn.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+
+                String status_value = mStatus.getText().toString();
 
 
+
+                Intent status_intent = new Intent(SettingsActivity.this,StatusActivity.class);
+                status_intent.putExtra("status_value",status_value);
+                startActivity(status_intent);
+            }
+        });
+
+
+        mImageBtn.setOnClickListener(new View.OnClickListener() {
+
+
+
+
+
+
+            public void onClick(View v) {
+                Intent galleryIntent = new Intent();
+                galleryIntent.setType("image/*");
+                galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
+
+
+                startActivityForResult(Intent.createChooser(galleryIntent,"SELECT IMAGE"),GALLERY_PICK);
+            }
+        });
 
     }
 }
